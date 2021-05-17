840c72fe28a054e10a5269bb7dc161ee0122f473
liduo1996 <799975650@qq.com>
2021-05-17 14:01:10 +0800

    框架搭建和环境配置，访问简单测试-liduo

---
+++ springboot-6001/src/main/java/com/springboot/Springboot6001Application.java
@@ -0,0 +1,15 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot6001Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Springboot6001Application.class, args);
+        System.out.println("启动成功");
+    }
+
+}
---
+++ springboot-6001/src/main/java/com/springboot/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.springboot.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Description: 首页控制器
+ */
+@Controller
+public class IndexController {
+    //通过get方式请求路径
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
